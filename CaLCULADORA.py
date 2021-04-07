1d8b5a21e9216b42b70920d6efa3d9899560e00f
Galletitaw <73406168+LaGalletita@users.noreply.github.com>
2021-04-07 15:31:38 -0300

    Update CaLCULADORA.py
    I added elif XD

--- CaLCULADORA.py
+++ CaLCULADORA.py
@@ -1,4 +1,3 @@
-#Here the code request you to choose an option
 Operación = input("Elige una opción 1. Sumar 2. Restar 3. Multiplicar 4. Dividir: ")
 
 #Here are the different options and code request you the value for the numbers that you want to calculate
@@ -7,25 +6,23 @@ if Operación == ("1"):
     Valor2 = int(input("Escribe tu segundo valor: "))
 
     print(Valor1 + Valor2)
-if Operación == ("2"):
+elif Operación == ("2"):
     Valor1 = int(input("Escribe tu primer valor: "))
     Valor2 = int(input("Escribe tu segundo valor: "))
 
     print(Valor1 - Valor2)
-if Operación == ("3"):
+elif Operación == ("3"):
     Valor1 = int(input("Escribe tu primer valor: "))
     Valor2 = int(input("Escribe tu segundo valor: "))
 
     print(Valor1 * Valor2)
-if Operación == ("4"):
+elif Operación == ("4"):
     Valor1 = int(input("Escribe tu primer valor: "))
     Valor2 = int(input("Escribe tu segundo valor: "))
 
     print(Valor1 / Valor2)
     
 #And here the code tells you that you need to choose the correct option if you hadn't chosen it correctly.
+
 else:
     print("Elige una de las opciones anteriormente mencionadas.")
-
-
-
