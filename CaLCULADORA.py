2e325c9a152acd7cc575d1decbecaf7b62d80e95
Galletitaw <73406168+LaGalletita@users.noreply.github.com>
2021-04-07 20:01:47 -0300

    Calculadora XD
    Una calculadora que calcula 😎

--- CaLCULADORA.py
+++ CaLCULADORA.py
@@ -1,34 +1,41 @@
 #Here the code request you to choose an option
-Operación = input("Elige una opción 1. Sumar 2. Restar 3. Multiplicar 4. Dividir: ")
+Operación = input("Elige una opción 1. Sumar 2. Restar 3. Multiplicar 4. Dividir 5. Potenciación: ")
 
 #Here are the different options and code request you the value for the numbers that you want to calculate
 
-#ADD
+#--ADD--
 if Operación == ("1"):
     Valor1 = int(input("Escribe tu primer valor: "))
     Valor2 = int(input("Escribe tu segundo valor: "))
     int= Valor1 + Valor2
     print("Tu resultado es",int)
 
-#ABSTRACT
+#--ABSTRACT--
 elif Operación == ("2"):
     Valor1 = int(input("Escribe tu primer valor: "))
     Valor2 = int(input("Escribe tu segundo valor: "))
     int= Valor1 - Valor2
     print("Tu resultado es",int)
 
-#MULTIPLICATE
+#--MULTIPLICATE--
 elif Operación == ("3"):
     Valor1 = int(input("Escribe tu primer valor: "))
     Valor2 = int(input("Escribe tu segundo valor: "))
     int = Valor1 * Valor2
     print("Tu resultado es",int)
 
-#DIVIDE
+#--DIVIDE--
 elif Operación == ("4"):
     Valor1 = int(input("Escribe tu primer valor: "))
     Valor2 = int(input("Escribe tu segundo valor: "))
     int = Valor1 / Valor2
+    print("Tu resultado es",int)
+
+#--POTENTIATION--
+elif Operación == ("5"):
+    Valor1= int(input("Escribe tu primer valor: "))
+    Valor2= int(input("Escribe la potenciación: "))
+    int = Valor1 ** Valor2
     print("Tu resultado es",int)
 
 #And here the code tells you that you need to choose the correct option if you hadn't chosen it correctly.
